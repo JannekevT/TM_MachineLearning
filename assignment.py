98acad8f59a4d858d1ad322c4ac6c0ddc2ddf35d
larsjongsma <lars@jongsma.email>
2026-03-03 14:31:21 +0100

    3-3

--- assignment.py
+++ assignment.py
@@ -8,6 +8,8 @@ from worclipo.load_data import load_data
 #from worcliver.load_data import load_data
 #from hn.load_data import load_data
 #from ecg.load_data import load_data
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 data = load_data()
 print(f'The number of samples: {len(data.index)}')
@@ -22,3 +24,28 @@ print(f'The number of liposarcoma: {(data["label"] == "liposarcoma").sum()}')
 #number of missing values
 print("Number of total missing values:", data.isnull().sum().sum())
 print("Number of feature missing values:", data.iloc[:, 2:].isnull().sum().sum())
+
+#%%
+#split data into train and test sets
+x = data.drop(["label"], axis='columns').values
+y = data["label"].map({'lipoma':0, 'liposarcoma':1})
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15, random_state=42, stratify = y)
+
+#plot data
+fig = plt.figure(figsize=(24,8))
+
+ax = fig.add_subplot(131)
+ax.set_title("entire dataset", fontsize='small')
+ax.scatter(x[:, 0], x[:, 1], marker='o', c=y,
+           s=25, edgecolor='k', cmap=plt.cm.Paired)
+ax = fig.add_subplot(132)
+ax.set_title("Training data", fontsize='small')
+ax.scatter(x_train[:, 0], x_train[:, 1], marker='o', c=y_train,
+           s=25, edgecolor='k', cmap=plt.cm.Paired)
+
+ax = fig.add_subplot(133)
+ax.set_title("Test data", fontsize='small')
+ax.scatter(x_test[:, 0], x_test[:, 1], marker='o', c=y_test,
+           s=25, edgecolor='k', cmap=plt.cm.Paired)
+ 
+# %%
